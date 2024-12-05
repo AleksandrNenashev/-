cb0e2dc05e0eca6a981f81c2fb6414521f93ad3b
Александр Ненашев <nenashev.a@rscable.ru>
2024-12-06 01:53:22 +0400

    fix header and spec offers

--- components/bitrix/news.list/spec_offer_mob/template.php
+++ components/bitrix/news.list/spec_offer_mob/template.php
@@ -20,9 +20,6 @@ $this->setFrameMode(true);
 <?endif;?>
 <?foreach($arResult["ITEMS"] as $arItem):?>
 	<?
-	echo "<pre>";
-	var_dump($arItem);
-	echo "</pre>";
 	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
 	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
 	?>
